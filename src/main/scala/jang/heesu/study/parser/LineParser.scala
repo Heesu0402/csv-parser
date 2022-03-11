dc2354dcb45e7cde870962a18aec616b74059fac
Heesu0402 <37855617+Heesu0402@users.noreply.github.com>
2022-03-11 14:06:47 +0900

    Update LineParser.scala
    LineParser 에 대한 추가 설명

--- src/main/scala/jang/heesu/study/parser/LineParser.scala
+++ src/main/scala/jang/heesu/study/parser/LineParser.scala
@@ -2,6 +2,13 @@ package jang.heesu.study.parser
 
 import com.sun.org.apache.bcel.internal.generic.ArrayType
 
+
+//TODO Line Parser가 여러개일 이유가 있는가? Object로 선언하면 안되는 이유
+/**
+   object 로 선언하는 경우 singleton 객체(동반객체)
+   인스턴스가 오직 1개만 생성 = 전역적으로 접근 가능(데이터공유 쉬움) + 메모리 이득
+   FileParser 는 데이터를 불러오는 부분으로 object 로 만들어서 사용
+ */
 class LineParser {
   /**
    * 분석파일을 루프 돌며 배열에 넣는 기능
